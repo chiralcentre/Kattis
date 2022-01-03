7b84fcfb31bdede8d59e0f717577e13ac77d6db3
Ryan Tan <83827461+chiralcentre@users.noreply.github.com>
2022-01-03 21:19:43 +0800

    +

--- exactlyelectrical.py
+++
@@ -1,9 +0,0 @@
-a,b = map(int,input().split())
-c,d = map(int,input().split())
-t = int(input())
-manhattan_d = abs(a-c) + abs(b-d)
-if manhattan_d > t:
-    print('N')
-else:
-    #print yes if difference between manhattan distance and electrical charge is a multiple of 2, as this means back and forth trips can be made
-    print('Y') if not (t-manhattan_d)%2 else print('N') 
