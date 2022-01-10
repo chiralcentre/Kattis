c0e25b3d79319d348464240b2f362df29a7bf751
Ryan Tan <83827461+chiralcentre@users.noreply.github.com>
2022-01-10 23:22:47 +0800

    +

---
+++ addtwonumbers.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class addtwonumbers {
+    public static void main(String args[]){
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt(), b = s.nextInt();
+        System.out.println(a+b);
+    }  
+}
