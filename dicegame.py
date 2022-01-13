d3cfe3b9daee7a859579fe722a3b319e74c72430
Ryan Tan <83827461+chiralcentre@users.noreply.github.com>
2022-01-13 22:08:27 +0800

    +

---
+++ dicegame.py
@@ -0,0 +1,5 @@
+a1,b1,a2,b2 = map(int,input().split())
+a3,b3,a4,b4 = map(int,input().split())
+# both probabilitiy distributions are symmetric around the mean.
+# Therefore it’s enough to compare the expected values of both probability distributions – compare the sum of both lines of input.
+print('Gunnar') if a1 + b1 + a2 + b2 > a3 + b3 + a4 + b4 else print('Emma') if a1 + b1 + a2 + b2 < a3 + b3 + a4 + b4 else print('Tie')
