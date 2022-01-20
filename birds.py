4974e6eb4ff37af4ae93b5dde3ce04710d5ba454
Ryan Tan <83827461+chiralcentre@users.noreply.github.com>
2022-01-20 12:08:37 +0800

    +

---
+++ birds.py
@@ -0,0 +1,10 @@
+L,D,N = map(int,input().split())
+birds,extra = sorted([int(input()) for _ in range(N)]),0
+if N > 0:
+    extra += (birds[0]-6)//D + (L-6-birds[-1])//D
+    for i in range(N-1):
+        if (birds[i+1] - birds[i]) >= 2*D:
+            extra += (birds[i+1] - birds[i])//D - 1
+else:
+    extra += (L-12)//D + 1
+print(extra)
