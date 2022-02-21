f8adca09de9574c5ea327a793af886f746358b34
Ryan Tan <83827461+chiralcentre@users.noreply.github.com>
2022-02-21 15:42:31 +0800

    +

---
+++ triangle.py
@@ -0,0 +1,12 @@
+from sys import stdin,stdout
+from math import floor,log10
+# Perimeter of a Sierpinski triangle of order (n) is 3^(n+1)/2^n
+def SierpinskiPerimeter(n):
+    return (3**(n+1))//(2**n) #returns integer part
+
+def numOfDigits(n):
+    return 1 + floor(log10(n))
+
+for linenum,line in enumerate(stdin):
+    stdout.write(f"Case {linenum+1}: {numOfDigits(SierpinskiPerimeter(int(line)))}\n")
+
