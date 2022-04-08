a36c0053602aff0bad80d072ec2be1b836917077
Ryan Tan <83827461+chiralcentre@users.noreply.github.com>
2022-04-08 22:26:20 +0800

    +

---
+++ islandhopping.py
@@ -0,0 +1,30 @@
+from sys import stdin,stdout
+from math import sqrt
+
+INF = 10**9 # use one billion to represent infinity
+for _ in range(int(stdin.readline())): #Prim's variant for dense graphs is used
+    m = int(stdin.readline())
+    points = [tuple(map(float,stdin.readline().split())) for _ in range(m)]
+    A,taken = [(INF,v) for v in range(m)],[False for _ in range(m)]
+    A[0] = (0,0) #start from first point
+    taken[0] = True; cost = 0; counter = 1
+    while counter < m: #O(m)
+        lowest,v = INF,0 # scan A to get v where A[v][0] is minimum in A
+        for i in range(m): #O(m)
+            if A[i][0] < lowest:
+                lowest = A[i][0]
+                v = i
+        #add vertices to taken
+        if not taken[v]:
+            taken[v] = True
+            counter += 1
+        if not taken[A[v][1]]:
+            taken[A[v][1]] = True
+            counter += 1
+        cost += A[v][0]
+        A[v] = (INF,A[v][1]) #prevent picking the same point
+        for j in range(m): #O(m)
+            if not taken[j] and A[j][0] > sqrt((points[v][0]-points[j][0])**2+(points[v][1]-points[j][1])**2):
+                A[j] = (sqrt((points[v][0]-points[j][0])**2+(points[v][1]-points[j][1])**2),v)
+    stdout.write(f'{cost}\n')
+    
