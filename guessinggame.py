09575052e89dd492190fcc21b15541cc7c7d3434
Ryan Tan <83827461+chiralcentre@users.noreply.github.com>
2022-05-11 21:43:23 +0800

    +

---
+++ guessinggame.py
@@ -0,0 +1,17 @@
+from sys import stdin,stdout
+
+lower,higher,honest = 0,11,True
+while True:
+    number = int(stdin.readline())
+    if number == 0:
+        break
+    response = stdin.readline().strip()
+    if response == "right on":
+        stdout.write("Stan may be honest\n") if lower < number < higher and honest else stdout.write("Stan is dishonest\n")
+        lower,higher,honest = 0,11,True #reset
+    elif response == "too high":
+        higher = min(higher,number) #take the tighter upper bound
+    elif response == "too low":
+        lower = max(lower,number)
+            
+    
