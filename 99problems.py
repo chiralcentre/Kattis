79d44f6320c01c82b49ee68bb2f6d16dcc49fa4e
Ryan Tan <83827461+chiralcentre@users.noreply.github.com>
2022-09-27 22:13:56 +0800

    +

--- 99problems.py
+++
@@ -1,8 +0,0 @@
-N = int(input())
-if N < 100:
-    print(99)
-else:
-    multiple = N//100
-    lower = (multiple-1)*100+99
-    upper = multiple*100+99
-    print(lower) if N - lower < upper - N else print(upper)
