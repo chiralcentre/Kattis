8bcb5a9b2e3452a1bd0baed8a043fa650b122eea
Ryan Tan <83827461+chiralcentre@users.noreply.github.com>
2022-12-14 22:28:58 +0800

    +

---
+++ howl.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    string howl;
+    getline(cin,howl);
+    cout << "AWH";
+    for (int i = 0; i < howl.length(); i++) cout << "O";
+    cout << "\n";
+    return 0;
+}
