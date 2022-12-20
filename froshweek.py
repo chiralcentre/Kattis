c3a07c10d6ee0075a36d4722827a028e1689d01a
Ryan Tan <83827461+chiralcentre@users.noreply.github.com>
2022-12-20 22:09:11 +0800

    +

---
+++ froshweek.py
@@ -0,0 +1,41 @@
+from sys import stdin,stdout,setrecursionlimit
+
+#increase recursion limit to prevent stack overflow
+setrecursionlimit(10**5)
+
+#minimum number of adjacent swaps = number of inversions
+#code runs in O(n log n) where n is length of string
+def mergeAndCountInv(start,mid,end,arr):
+    size = end - start + 1
+    #use auxillary array to store final sorted subarray
+    sortedArr = [-1 for _ in range(size)]
+    invCount,leftIdx,rightIdx,idx = 0,start,mid+1,0
+    #merge two sorted arrays into one sorted array
+    while leftIdx <= mid and rightIdx <= end:
+        if arr[leftIdx] <= arr[rightIdx]:
+            sortedArr[idx] = arr[leftIdx]
+            leftIdx += 1
+            invCount += rightIdx - mid - 1
+        else:
+            sortedArr[idx] = arr[rightIdx]
+            rightIdx += 1
+        idx += 1
+    while leftIdx <= mid:
+        sortedArr[idx] = arr[leftIdx]
+        idx += 1; leftIdx += 1
+        invCount += end - mid
+    while rightIdx <= end:
+        sortedArr[idx] = arr[rightIdx]
+        idx += 1; rightIdx += 1
+    for i in range(start,end+1):
+        arr[i] = sortedArr[i-start]
+    return invCount
+
+def sortAndCountInv(start,end,arr):
+    if start == end:
+        return 0
+    mid = start + (end - start) // 2
+    return sortAndCountInv(start,mid,arr) + sortAndCountInv(mid+1,end,arr) + mergeAndCountInv(start,mid,end,arr)
+
+line = [int(stdin.readline()) for _ in range(int(stdin.readline()))]
+stdout.write(str(sortAndCountInv(0,len(line)-1,line)))
