f3a50cf7dd2a183d55b68341b39d4f6740fa5218
Ryan Tan <83827461+chiralcentre@users.noreply.github.com>
2022-12-22 23:18:26 +0800

    +

---
+++ unionfind.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+#include <string>
+
+using namespace std;
+
+typedef vector<int> vi;
+
+class UnionFind {
+    private:
+    vi p, rank, setSize;
+    int numSets;
+
+    public:
+    UnionFind(int N) {
+        p.assign(N,0);
+        for (int i = 0; i < N; i++) p[i] = i;
+        setSize.assign(N,1);
+        rank.assign(N,0);
+        numSets = N;
+    }
+
+    int findSet(int i) {
+        if (p[i] == i) return i;
+        else {
+            p[i] = findSet(p[i]);
+            return p[i];
+        }
+    }
+
+    bool isSameSet(int i, int j) {return findSet(i) == findSet(j);}
+
+    void unionSet(int i, int j) {
+        if (!isSameSet(i,j)) {
+            int x = findSet(i), y = findSet(j);
+            if (rank[x] > rank[y]) swap(x,y);
+            p[x] = y;
+            if (rank[x] == rank[y]) rank[y]++;
+            setSize[y] += setSize[x];
+            numSets--;
+        }
+    }
+
+    int numOfDisjointSets() {return numSets;}
+
+    int sizeOfSet(int i) {return setSize[findSet(i)];}
+};
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int N,Q; cin >> N >> Q;
+    UnionFind UFDS(N);
+    while (Q--) {
+        string c; int i,j; cin >> c >> i >> j;
+        if (c == "=") UFDS.unionSet(i,j);
+        else cout << (UFDS.isSameSet(i,j) ? "yes\n" : "no\n");
+    }
+    return 0;
+}
