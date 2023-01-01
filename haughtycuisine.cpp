245e8341d21350f0cfd82de3a50f90ecf1ffb3ed
Ryan Tan <83827461+chiralcentre@users.noreply.github.com>
2023-01-01 18:06:17 +0800

    +

---
+++ haughtycuisine.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include <string>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n; cin >> n;
+    int d; cin >> d;
+    cout << d << "\n";
+    for (int i = 0; i < d; i++) {
+        string item; cin >> item;
+        cout << item << "\n";
+    }
+    return 0;
+}
