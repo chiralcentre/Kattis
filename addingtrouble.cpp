d1771617a3992169beeeb4b97da344e3a9d032f1
Ryan Tan <83827461+chiralcentre@users.noreply.github.com>
2023-04-03 22:03:29 +0800

    +

---
+++ addingtrouble.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int A,B,C; scanf("%d %d %d",&A,&B,&C);
+    (A + B == C) ? printf("correct!\n") : printf("wrong!\n");
+    return 0;
+}
