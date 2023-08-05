0591adec76cf6f4fdf5d5faafacc6d8630304ce7
Ryan Tan <83827461+chiralcentre@users.noreply.github.com>
2023-08-05 09:55:14 +0800

    +

---
+++ powerstrings.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int MAX_N = 2000001;
+
+string T, P;                       // T = text, P = pattern
+
+// credits to CP4 repository
+int b[MAX_N];                                    
+
+void kmpPreprocess() {                    
+    int i = 0, j = -1; b[0] = -1;                 
+    while (i < P.length()) {                                
+        while ((j >= 0) && (P[i] != P[j])) j = b[j]; 
+        b[++i] = ++j; 
+    }
+}
+
+int kmpSearch() {                               
+    int freq = 0, i = 0, j = 0;                              
+    while (i < T.length()) {                                
+        while ((j >= 0) && (T[i] != P[j])) j = b[j]; 
+        ++i; ++j;                                    
+        if (j == P.length()) {                                
+        ++freq;
+        j = b[j];                                 
+        }
+    }
+    return freq;
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    while (true) {
+        cin >> P;
+        if (P == ".") break;
+        T = P + P;
+        for (int i = 0; i < P.length(); i++) b[i] = 0;
+        kmpPreprocess();
+        // find number of occurrences of s in s + s, subtract 1
+        cout << kmpSearch() - 1 << "\n";
+    }
+    return 0;
+}
