10cdeb2342da9072c47d071c8265db0f23d4feb0
Ryan Tan <83827461+chiralcentre@users.noreply.github.com>
2024-01-19 13:17:19 +0800

    +

---
+++ aleidibio.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+ll a,b,c;
+
+int main() {
+    scanf("%lld\n%lld\n%lld",&a,&b,&c);
+    printf("%lld\n",c - a - b);
+    return 0;
+}
