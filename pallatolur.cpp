0d88a00f076a3ee6d7399ccd92e2115ee5ba4600
Ryan Tan <83827461+chiralcentre@users.noreply.github.com>
2024-01-27 17:03:50 +0800

    +

---
+++ pallatolur.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int a,b;
+
+int main() {
+    scanf("%d\n%d",&a,&b);
+    if (a <= 2 && 2 <= b) printf("1\n2\n");
+    else printf(":(\n");
+    return 0;
+}
