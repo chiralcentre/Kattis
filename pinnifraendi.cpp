835d2e3fc7e52b1c7474ae2e22d382694f27af53
Ryan Tan <83827461+chiralcentre@users.noreply.github.com>
2024-01-28 15:10:48 +0800

    +

---
+++ pinnifraendi.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+
+int main() {
+    scanf("%d",&n);
+    printf("0.");
+    for (int i = 0; i < n - 1; i++) printf("0");
+    printf("1\n");
+    return 0;
+}
