02b60f25e67180015e983cea09ced5012917cf41
Ryan Tan <83827461+chiralcentre@users.noreply.github.com>
2024-01-29 21:58:27 +0800

    +

---
+++ reduplikation.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string s; int n;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin >> s >> n;
+    for (int i = 0; i < n; i++) cout << s;
+    return 0;
+}
