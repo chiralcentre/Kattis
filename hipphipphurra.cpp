99bf40c2d3fdbadd75e42773edb79cdc70631042
Ryan Tan <83827461+chiralcentre@users.noreply.github.com>
2024-05-03 10:21:26 +0800

    +

---
+++ hipphipphurra.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string s; int x;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin >> s;
+    cin >> x;
+    for (int i = 0; i < x; i++) cout << "Hipp hipp hurra, " << s << "!\n";
+    return 0;
+}
