9f5e2cfabebc1bfe82475b9a9f967644b66868db
Ryan Tan <83827461+chiralcentre@users.noreply.github.com>
2024-05-12 15:22:48 +0800

    +

---
+++ takkfyrirmig.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n; string s;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> s;
+        cout << "Takk " << s << "\n";
+    }
+    return 0;
+}
