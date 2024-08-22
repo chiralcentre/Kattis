f846da6debc1e0b378b527fae111d8c340626a27
Ryan Tan <83827461+chiralcentre@users.noreply.github.com>
2024-08-22 19:17:50 +0800

    +

---
+++ skak.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int xh,yh,xp,yp;
+
+int main() {
+    scanf("%d %d\n%d %d",&xh,&yh,&xp,&yp);
+    xh != xp && yh != yp ? printf("2\n") : printf("1\n");
+    return 0;
+}
