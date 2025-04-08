0d9ecebde7ed29c77756c05dd5ecf466e6b89d38
chiralcentre <ryantan111100@gmail.com>
2025-04-08 16:23:28 +0800

    +

---
+++ skeytasaman.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string line1, line2;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    getline(cin, line1);
+    getline(cin, line2);
+    cout << line1 << line2;
+    return 0;
+}
