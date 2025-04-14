d20ccfb7543c30e81821c98ea9733f30bbfabb80
Ryan Tan <83827461+chiralcentre@users.noreply.github.com>
2025-04-14 22:14:51 +0800

    +

--- vegabrefadagsetningar.py
+++
@@ -1,8 +0,0 @@
-mappings = {"JAN": "01", "FEB": "02", "MAR": "03", "APR": "04",
-            "MAY": "05", "JUN": "06", "JUL": "07", "AUG": "08",
-            "SEP": "09", "OCT": "10", "NOV": "11", "DEC": "12"
-            }
-
-d,m,m2,y = input().split()
-m2 = m2[1:]
-print(f"20{y}-{mappings[m2]}-{d}")
