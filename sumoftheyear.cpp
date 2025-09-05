2ea79418d571da729fd9f369bd989ef1575f85d9
Ryan Tan <83827461+chiralcentre@users.noreply.github.com>
2025-09-05 23:32:15 +0800

    +

---
+++ sumoftheyear.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+
+// square of sum = sum of cubes for all numbers
+int main() {
+    scanf("%d",&n);
+    printf("%d\n",n);
+    return 0;
+}
