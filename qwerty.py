b8e72b97fea205e56a1871e306583f7ec1d635b9
chiralcentre <ryantan111100@gmail.com>
2025-10-16 18:19:16 +0800

    +

---
+++ qwerty.py
@@ -0,0 +1,12 @@
+from sys import stdin,stdout
+
+mappings = {
+    'a': 'q', 'b': 'w', 'c': 'e', 'd': 'r', 'e': 't', 'f': 'y', 'g': 'u', 'h': 'i', 'i': 'o', 'j': 'p',
+    'k': 'a', 'l': 's', 'm': 'd', 'n': 'f', 'o': 'g', 'p': 'h', 'q': 'j', 'r': 'k', 's': 'l',
+    't': 'z', 'u': 'x', 'v': 'c', 'w': 'v', 'x': 'b', 'y': 'n', 'z': 'm'
+}
+
+stdin.readline()
+words = stdin.readline().strip()
+for char in words:
+    stdout.write(mappings.get(char," "))
