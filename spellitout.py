7d0ba4e24949de7da2960971c40e71e526cb110e
chiralcentre <ryantan111100@gmail.com>
2026-04-04 18:29:34 +0800

    +

---
+++ spellitout.py
@@ -0,0 +1,42 @@
+number_to_words = {
+    0: "zero",
+    1: "one",
+    2: "two",
+    3: "three",
+    4: "four",
+    5: "five",
+    6: "six",
+    7: "seven",
+    8: "eight",
+    9: "nine",
+    10: "ten",
+    11: "eleven",
+    12: "twelve",
+    13: "thirteen",
+    14: "fourteen",
+    15: "fifteen",
+    16: "sixteen",
+    17: "seventeen",
+    18: "eighteen",
+    19: "nineteen",
+    20: "twenty",
+    30: "thirty",
+    40: "forty",
+    50: "fifty",
+    60: "sixty",
+    70: "seventy",
+    80: "eighty",
+    90: "ninety"
+}
+
+N = int(input())
+if N <= 20:
+    print(number_to_words[N])
+else:
+    digit = N % 10
+    tens = N - digit
+    if digit == 0:
+        print(f"{number_to_words[tens]}")
+    else:
+        print(f"{number_to_words[tens]}-{number_to_words[digit]}")
+    
