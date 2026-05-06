6a63be3b737851d00158988d0f1815199c576d6d
Ryan Tan <83827461+chiralcentre@users.noreply.github.com>
2026-05-06 23:48:43 +0800

    +

---
+++ vinabond.py
@@ -0,0 +1,42 @@
+from sys import stdin,stdout
+
+class UnionFind:
+    def __init__(self,N):
+        self.p = [i for i in range(N)]
+        self.rank = [0 for i in range(N)]
+        self.setSize = [1 for i in range(N)]
+        self.numSets = N
+
+    def findSet(self,j):
+        if self.p[j] == j:
+            return j
+        else:
+            self.p[j] = self.findSet(self.p[j])
+            return self.p[j]
+
+    def isSameSet(self,i,j):
+        return self.findSet(i) == self.findSet(j)
+
+    def unionSet(self,i,j):
+        if not self.isSameSet(i,j):
+            x,y = self.findSet(i),self.findSet(j)
+            # invariant: rank(x) <= rank(y)
+            if self.rank[x] > self.rank[y]:
+                x,y = y,x
+            self.p[x] = y
+            if self.rank[x] == self.rank[y]:
+                self.rank[y] += 1
+            self.setSize[y] += self.setSize[x]
+            self.numSets -= 1
+
+    def sizeOfSet(self,i):
+        return self.setSize[self.findSet(i)]
+
+N = int(stdin.readline())
+UFDS = UnionFind(N + 1)
+for i in range(N):
+    a,b = map(int,stdin.readline().split())
+    UFDS.unionSet(a,b)
+for _ in range(int(stdin.readline())):
+    a,b = map(int,stdin.readline().split())
+    stdout.write("Vinir!\n") if a != b and UFDS.isSameSet(a,b) else stdout.write("Kannski?\n")
