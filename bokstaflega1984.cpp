eee738284d2e159518436744b5c62fb45f1334aa
Ryan Tan <83827461+chiralcentre@users.noreply.github.com>
2026-06-26 23:02:35 +0800

    +

---
+++ bokstaflega1984.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+
+int main() {
+    scanf("%d",&n);
+    (n == 1984) ? printf("Literally 1984\n") : printf("Not 1984... yet\n");
+    return 0;
+}
